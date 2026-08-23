e0bc3416e586fd7ffee94160dee0c2e40500d336
Michael Chisari <michael@chisari.us>
2026-08-23 04:19:43 -0400

    move parser to its own file

--- batch.go
+++ batch.go
@@ -715,12 +715,6 @@ func validateQuery(query string) error {
 	return nil
 }
 
-// lexicalParser returns the SQL reserved words found in query and reports
-// whether query contains multiple statements.
-func lexicalParser(query string) (words []string, multiple bool) {
-	return []string{}, false
-}
-
 /*
 func main() {
   batch := NewBatchqlite()
---
+++ parser.go
@@ -0,0 +1,7 @@
+package batchqlite
+
+// lexicalParser returns the SQL reserved words found in query and reports
+// whether query contains multiple statements.
+func lexicalParser(query string) (words []string, multiple bool) {
+	return []string{}, false
+}
