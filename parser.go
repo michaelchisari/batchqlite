4efca7842ae9b3e9cdaf2872465cbd22559e6f9a
Michael Chisari <michael@chisari.us>
2026-08-23 04:50:43 -0400

    Added illegal keywords

--- parser.go
+++ parser.go
@@ -1,5 +1,21 @@
 package batchqlite
 
+var illegalKeywords = []string{
+	"BEGIN",
+	"COMMIT",
+	"END",
+	"ROLLBACK",
+	"SAVEPOINT",
+	"RELEASE",
+	"ATTACH",
+	"DETACH",
+	"PRAGMA",
+	"RETURNING",
+	"VACUUM",
+	"REINDEX",
+	"ANALYZE",
+}
+
 // lexicalParser returns the SQL reserved words found in query and reports
 // whether query contains multiple statements.
 func lexicalParser(query string) (words []string, multiple bool) {
